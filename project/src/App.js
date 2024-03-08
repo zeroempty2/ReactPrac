2ae832027948950edee4856398e2b461b77a9543
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-03-08 21:34:29 +0900

    라우터 작성

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes,Route,Link} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+    <Link to = "/"> Home </Link>
+    <Link to = "/about"> About </Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
     </div>
   );
 }
 
 export default App;
+//Routes 안에 어떤 컴포넌트를 정의해서 보여줄지 결정할 수 있음
+// Link -> Route -> element 
+// <Link to = "/"> 는 <a href = "/" > 와 같음
