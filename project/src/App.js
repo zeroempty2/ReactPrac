eb67725da6b599be18072c4d174bef14401a53da
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-03-13 21:20:36 +0900

    Review페이지 임시 작성

--- project/src/App.js
+++ project/src/App.js
@@ -6,13 +6,15 @@ import Counter from "./pages/Counter";
 import Input from "./pages/Input";
 import Input2 from "./pages/Input2";
 import List from "./pages/List";
-
+import ReviewList from "./pages/ReviewList";
+import Review from "./pages/Review";
 function App() {
   return (
     <div className="App">
     <nav>
     <Link to = "/"> Home </Link> | <Link to = "/about"> About </Link> | <Link to = "/counter"> Counter </Link> | 
-    <Link to = "/input"> Input </Link> | <Link to = "/input2"> Input2 </Link> | <Link to = "/list"> List </Link>
+    <Link to = "/input"> Input </Link> | <Link to = "/input2"> Input2 </Link> | <Link to = "/list"> List </Link> |
+    <Link to = "/reviewList/*"> ReviewList </Link> 
     </nav>
     <Routes>
       <Route path="/" element={<Home />} />
@@ -21,6 +23,8 @@ function App() {
       <Route path="/input" element={<Input />} />
       <Route path="/input2" element={<Input2 />} />
       <Route path="/list" element={<List />} />
+      <Route path="/reviewList/*" element={<ReviewList />} />
+      <Route path="/review" element={<Review />} />
     </Routes>
     </div>
   );
